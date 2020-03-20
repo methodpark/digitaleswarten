7598d29b41291ca3b5da9791c3bd6c19d14ade5b
Gronner <felix.braeunling@t-online.de>
2020-03-20 23:23:51 +0100

    Add database support

--- backend/app.py
+++ backend/app.py
@@ -1,22 +1,7 @@
 # flask_web/app.py
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
-
-Slot_Counter = 0
-
-@app.route('/')
-def hello_world():
-    return 'Hey, we have Flask in a Docker container!'
-
-
-@app.route('/slot')
-def print_slot():
-    global Slot_Counter
-    temp = Slot_Counter
-    Slot_Counter += 1
-    return str(temp)
-
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////db/digitalesWartenDB.sqlite'
+db = SQLAlchemy(app)
