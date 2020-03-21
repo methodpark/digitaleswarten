a43be338f466a71f9cc4a5e972729fa9a0d6ca50
Richard Baumann <ohaz@ohaz.engineer>
2020-03-21 08:15:30 +0100

    Add queue for slots

--- backend/models/slot.py
+++ backend/models/slot.py
@@ -2,6 +2,8 @@ from app import db
 
 class Slot(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    queue_id = db.Column(db.Integer, db.ForeignKey('queue.id'))
+    queue = db.relationship('Queue', backref=db.backref('slots'))
 
     def __repr__(self):
         return f'<Slot {id}>'
