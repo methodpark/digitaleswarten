04444dbba76970450085a6420c8748c02055cf63
Richard Baumann <ohaz@ohaz.engineer>
2020-03-21 15:36:05 +0100

    Add default place and add option to add queues

--- backend/digitales_warten.py
+++ backend/digitales_warten.py
@@ -1,19 +1,13 @@
 #!/usr/bin/env python3
-
+from flask import request, abort, jsonify
 from app import app, db
 from models.slot import Slot
 from models.queue import Queue
+from utils.id_generator import generate_queue_id
 
 @app.route('/')
 def hello_world():
     return 'Hey, we have Flask in a Docker container!'
-
-@app.route('/queue', methods=['POST'])
-def create_queue():
-    queue = Queue()
-    db.session.add(queue)
-    db.session.commit()
-    return str(queue.id)
 
 @app.route('/queue/<queue_id>/slot', methods=['POST'])
 def create_slot(queue_id):
@@ -22,6 +16,21 @@ def create_slot(queue_id):
     db.session.add(slot)
     db.session.commit()
     return str(slot.id)
+
+@app.route('/place/<place_id>/queues', methods=['POST'])
+def create_queue(place_id):
+    if 'application/json' not in request.headers['Content-Type']:
+        abort(400)
+    data = request.json
+    if 'queueName' not in data:
+        abort(400)
+    # Check password here
+    queueName = data['queueName']
+    queue_id = generate_queue_id(queueName)
+    queue = Queue(id=queue_id, name=queueName)
+    db.session.add(queue)
+    db.session.commit()
+    return jsonify(id=queue.id, name=queue.name)
 
 
 if __name__ == '__main__':
--- backend/models/queue.py
+++ backend/models/queue.py
@@ -1,7 +1,8 @@
 from app import db
 
 class Queue(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.String, primary_key=True)
+    name = db.Column(db.String)
 
     def __repr__(self):
         return f'<Queue {self.id}: [{self.slots}]>'
