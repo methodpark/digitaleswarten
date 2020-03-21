651b97edf3cbe1cfcebfbdca3dd493704774ae9b
Tilman Adler <Tilman.Adler@methodpark.de>
2020-03-21 16:34:06 +0100

    Remove unused Component from App tree

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import logo from './logo.svg';
-import Queue from './queue/Queue';
 import './App.css';
 
 function App() {
@@ -20,7 +19,6 @@ function App() {
           Learn React
         </a>
       </header>
-      <Queue />
     </div>
   );
 }
