550faff0ed0c321e38ab3145bc9536625dd035ff
Tobias Gutmann <tobias.gutmann@methodpark.de>
2020-03-22 00:41:14 +0100

    Add welcome patient view

---
+++ frontend/src/patient/PatientWelcome.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import virusLogo from '../img/wirvsvirus.png';
+
+export function PatientWelcome() {
+  return(
+    <div className="welcome-background-image">
+      <div className="welcome-patient-text-wrapper">
+        <h1 className="welcome-patient-headline">Digitales Wartezimmer</h1>
+        <p className="welcome-patient-subheadline">Bitte geben Sie die von Ihrer Praxis erhaltene Wartenummer in das untere Feld ein.</p>
+      </div>
+      <div className="welcome-input-wrapper">
+        <input
+          className="welcome-patient-id-input"
+          type="text"
+          name="patient-id"
+          placeholder="Ihre Wartenummer">
+        </input>
+        <div className="welcome-patient-button"></div>
+      </div>
+      <div className="welcome-patient-footer">
+        <img
+          className="welcome-patient-virus-logo"
+          src={virusLogo}
+          alt="Wir VS Virus Logo"/>
+      </div>
+    <div className="welcome-background-overlay"></div>
+  </div>
+  );
+}
+
+
