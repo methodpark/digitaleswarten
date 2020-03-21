50f9eae6808311ab4fcdd832683b8bce119e5076
Richard Baumann <ohaz@ohaz.engineer>
2020-03-21 08:15:30 +0100

    Add queue for slots

--- backend/digitales_warten.py
+++ backend/digitales_warten.py
@@ -1,14 +1,22 @@
 from app import app, db
 from models.slot import Slot
+from models.queue import Queue
 
 @app.route('/')
 def hello_world():
     return 'Hey, we have Flask in a Docker container!'
 
+@app.route('/queue', methods=['POST'])
+def create_queue():
+    queue = Queue()
+    db.session.add(queue)
+    db.session.commit()
+    return str(queue.id)
 
-@app.route('/slot')
-def print_slot():
-    slot = Slot()
+@app.route('/slot/<queue_id>', methods=['POST'])
+def print_slot(queue_id):
+    queue = Queue.query.filter_by(id=queue_id).first()
+    slot = Slot(queue=queue)
     db.session.add(slot)
     db.session.commit()
     return str(slot.id)
