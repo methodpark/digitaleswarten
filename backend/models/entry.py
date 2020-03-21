8f5f89b8b12d0607e1c969cbf30fcf19dff00fa0
Gronner <felix.braeunling@t-online.de>
2020-03-21 21:21:24 +0100

    Rename slot to entry

--- backend/models/entry.py
+++ backend/models/entry.py
@@ -1,6 +1,6 @@
 from app import db
 
-class Slot(db.Model):
+class Entry(db.Model):
     id = db.Column(db.String, primary_key=True)
     queue_id = db.Column(db.String, db.ForeignKey('queue.id'), primary_key=True)
     queue = db.relationship('Queue', backref=db.backref('slots'))
@@ -8,4 +8,4 @@ class Slot(db.Model):
     ticket_number = db.Column(db.Integer)
 
     def __repr__(self):
-        return f'<Slot {id}>'
+        return f'<Entry {id}>'
