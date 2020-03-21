73b2acac985dfd5d348a87683219f8cab4c11adf
Richard Baumann <ohaz@ohaz.engineer>
2020-03-21 19:51:28 +0100

    Add API to create slot

--- backend/models/slot.py
+++ backend/models/slot.py
@@ -2,8 +2,9 @@ from app import db
 
 class Slot(db.Model):
     id = db.Column(db.String, primary_key=True)
-    queue_id = db.Column(db.String, db.ForeignKey('queue.id'))
+    queue_id = db.Column(db.String, db.ForeignKey('queue.id'), primary_key=True)
     queue = db.relationship('Queue', backref=db.backref('slots'))
+    name = db.Column(db.String)
     ticket_number = db.Column(db.Integer)
 
     def __repr__(self):
