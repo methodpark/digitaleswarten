ffc0a87cddc53e7c5e5294bb748630888ed7bf6e
Tobias Schaefer <tssr@methodpark.de>
2020-03-22 14:00:41 +0100

    Removed dummy data

--- frontend/src/patient/QueueBoard.tsx
+++ frontend/src/patient/QueueBoard.tsx
@@ -6,89 +6,34 @@ import { Person } from '../model/person';
 import icon from '../img/arzt-icon.png';
 
 export function QueueBoard(props: AppState) {
-  console.log(props);
-  const { queues = [
-    {
-      id: 'queue1', name: 'notfall', entries: [
-        { name: "Heiko", id: 'notfall1', ticketNumber: 1 },
-        { name: "Lena", id: 'notfall2', ticketNumber: 2 },
-        { name: "SuperLongNameWTFIsHappeningHere", id: 'notfall3', ticketNumber: 3 }
-      ]
-    },
-    {
-      id: 'queue2', name: 'corona', entries: [
-        { id: 'corona1', name: 'Tobias', ticketNumber: 1 },
-        { id: 'corona1', name: 'Tobias', ticketNumber: 1 },
-        // { id: 'corona1', name: 'Tobias', ticketNumber: 1 },
-        // { id: 'corona1', name: 'Tobias', ticketNumber: 1 },
-        // { id: 'corona1', name: 'Tobias', ticketNumber: 1 },
-        // { id: 'corona1', name: 'Tobias', ticketNumber: 1 },
-        // { id: 'corona1', name: 'Tobias', ticketNumber: 1 },
-        // { id: 'corona1', name: 'Tobias', ticketNumber: 1 },
-        // { id: 'corona1', name: 'Tobias', ticketNumber: 1 },
-        // { id: 'corona1', name: 'Tobias', ticketNumber: 1 },
-        // { id: 'corona1', name: 'Tobias', ticketNumber: 1 },
-        // { id: 'corona1', name: 'Tobias', ticketNumber: 1 },
-        // { id: 'corona1', name: 'Tobias', ticketNumber: 1 },
-        // { id: 'corona1', name: 'Tobias', ticketNumber: 1 },
-        // { id: 'corona1', name: 'Tobias', ticketNumber: 1 },
-        // { id: 'corona1', name: 'Tobias', ticketNumber: 1 },
-        // { id: 'corona1', name: 'Tobias', ticketNumber: 1 },
-        // { id: 'corona1', name: 'Tobias', ticketNumber: 1 },
-        { id: 'corona1', name: 'Tobias', ticketNumber: 1 },
-        { id: 'corona1', name: 'Tobias', ticketNumber: 1 },
-        { id: 'corona1', name: 'Tobias', ticketNumber: 1 },
-        { id: 'corona1', name: 'Tobias', ticketNumber: 1 },
-        { id: 'corona1', name: 'Tobias', ticketNumber: 1 },
-        { id: 'corona2', name: 'Franz', ticketNumber: 2 },
-        { id: 'corona2', name: 'Franz', ticketNumber: 2 },
-        { id: 'corona1', name: 'Tobias', ticketNumber: 1 },
-        { id: 'corona1', name: 'Tobias', ticketNumber: 1 },
-        { id: 'corona2', name: 'Franz', ticketNumber: 2 },
-        { id: 'corona2', name: 'Franz', ticketNumber: 2 },
-        { id: 'corona2', name: 'Franz', ticketNumber: 2 },
-        { id: 'corona2', name: 'Franz', ticketNumber: 2 },
-        { id: 'corona2', name: 'Franz', ticketNumber: 2 },
-        { id: 'corona2', name: 'Franz', ticketNumber: 2 },
-        { id: 'corona2', name: 'Franz', ticketNumber: 2 },
-        { id: 'corona2', name: 'Franz', ticketNumber: 2 },
-        { id: 'corona2', name: 'Franz', ticketNumber: 2 },
-        { id: 'corona2', name: 'Franz', ticketNumber: 2 },
-        { id: 'corona2', name: 'Franz', ticketNumber: 2 },
-        { id: 'corona2', name: 'Franz', ticketNumber: 2 },
-        { id: 'corona2', name: 'Franz', ticketNumber: 2 },
-        { id: 'corona2', name: 'Franz', ticketNumber: 2 },
-        { id: 'corona2', name: 'Franz', ticketNumber: 2 },
-        { id: 'corona2', name: 'Franz', ticketNumber: 2 },
-        { id: 'corona2', name: 'Franz', ticketNumber: 2 },
-        { id: 'corona2', name: 'Franz', ticketNumber: 2 },
-        { id: 'corona2', name: 'Franz', ticketNumber: 2 },
-        { id: 'corona2', name: 'Franz', ticketNumber: 2 },
-        { id: 'corona2', name: 'Franz', ticketNumber: 2 },
-        { id: 'corona2', name: 'Franz', ticketNumber: 2 },
-        { id: 'corona2', name: 'Franz', ticketNumber: 2 },
-      ]
-    }
-  ] } = props;
+  const { queues = [] } = props;
 
 
+
+  if (queues.length < 1) {
+    return (
+      <div className="queueboard-wrapper">
+        <div className="queueboard-patient-entry">
+          Queues are empty
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="queueboard-wrapper">
-        {queues.map((queue: Queue) => {
-          return queue.entries.map((entry: Person) => {
-            return (
+      {queues.map((queue: Queue) => {
+        return queue.entries.map((entry: Person) => {
+          return (
             <div key={entry.id} className="queueboard-patient-entry">{entry.id}
               <img alt="An icon showing a patient" src={icon}></img>
             </div>
-            )
-          })
-        })}
+          )
+        })
+      })}
     </div>
   );
 }
 
-
 const mapStateToProps = (state: AppState) => state;
-
 export default connect(mapStateToProps)(QueueBoard);
