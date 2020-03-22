5de24b4035b3d0194b9e8580f689b7dfa209c424
Tilman Adler <Tilman.Adler@methodpark.de>
2020-03-22 21:39:22 +0100

    Tweak Queue board page

--- frontend/src/patient/QueueBoard.tsx
+++ frontend/src/patient/QueueBoard.tsx
@@ -3,8 +3,9 @@ import { connect } from 'react-redux';
 import { AppState } from '../state/state';
 import { Queue } from '../model/queue';
 import { Person } from '../model/person';
-import icon from '../img/arzt-icon.png';
 import SelfRefreshingQueues from '../SelfRefreshingQueues';
+
+import '../styles/QueueBoard.css'
 
 export function QueueBoard(props: any) {
   const { queues = [] } = props;
@@ -13,7 +14,7 @@ export function QueueBoard(props: any) {
     return (
       <div className="queueboard-wrapper">
         <div className="queueboard-patient-entry">
-          Queues are empty
+          In diesem Wartebereich gibt es keine Warteschlangen. Haben Sie vielleicht die falsche Wartebereichsnummer gewählt?
         </div>
       </div>
     );
@@ -21,28 +22,33 @@ export function QueueBoard(props: any) {
 
   return (
     <>
-    <div className="queueboard-header">
-      <div>Willkommen bei digitales:warten</div>
-      <div>Sie befinden sich im Wartebereich von {props.placeId.charAt(0).toUpperCase() + props.placeId.slice(1)}</div>
-    </div>
+    <h1>Wartebereich {props.placeId}</h1>
+
+    <p>
+      Sie befinden Sich im Wartebereich mit der Nummer <b>{props.placeId}</b>.<br/>
+      Sie finden Ihre Ticketnummer in einem der unten stehenden Warteschlangen.
+      Wenn Ihr Ticket aufgerufen wird, wird es farblich markiert.</p>
+
     <div className="queueboard-wrapper">
       {queues.map((queue: Queue) => {
         return (
-          <div className="queue-wrapper">
-            <div className="queue-header">{queue.name}</div>
+          <section className="queue-wrapper">
+            <header className="queue-header">Warteschlange <br/> {queue.name}</header>
+            <div className="ticket-container">
             {queue.entries.map((entry: Person) => {
-              let patientMessage = <></>;
+              let patientMessage = <div>&nbsp;</div>;
               if(entry.state === "called") {
                 patientMessage = <div>Sie wurden aufgerufen.</div>;
               }
               return (
-                <div key={entry.id} className={"queueboard-patient-entry " + (entry.state === "called" ? "called" : "")}>{entry.ticketNumber}
+                <div key={entry.id} className={"queueboard-patient-entry " + (entry.state === "called" ? "called" : "")}>
+                  <span>Ticketnummer: <span className="ticket-number">{entry.ticketNumber}</span></span>
                   {patientMessage}
-                  <img alt="An icon showing a patient" src={icon}></img>
                 </div>
               )
             })}
-          </div>
+            </div>
+          </section>
         )
       })}
     </div>
