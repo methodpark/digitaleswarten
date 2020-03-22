a30d9753403ec0ee8211993779f6818bf4279867
Tilman Adler <Tilman.Adler@methodpark.de>
2020-03-22 17:59:10 +0100

    Rephrase welcome page

--- frontend/src/patient/PatientWelcome.tsx
+++ frontend/src/patient/PatientWelcome.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 
+import uuid from 'uuid/v4';
+
 import '../styles/Welcome.css';
 import virusLogo from '../img/wirvsvirus.png';
 
 export function PatientWelcome() {
+
   return(
-    <div className="welcome-background-image">
-      <div className="welcome-patient-text-wrapper">
+    <main className="welcome-background-image">
+      <header className="welcome-patient-text-wrapper">
         <h1 className="welcome-patient-headline">Digitales Nummern Ziehen</h1>
-        <p className="welcome-patient-subheadline">Sie haben zwei Nummern erhalten. Bitte geben Sie die Wartebereichsnummer hier ein:</p>
-      </div>
+        <p className="welcome-patient-subheadline">Sie haben zwei Nummern erhalten. <br /> <br />Bitte geben Sie die Wartebereichsnummer hier ein:</p>
+      </header>
+
       <div className="welcome-input-wrapper">
         <input
           className="welcome-patient-id-input"
@@ -19,14 +23,14 @@ export function PatientWelcome() {
         </input>
         <div className="welcome-patient-button"></div>
       </div>
-      <div className="welcome-patient-footer">
+      <footer className="welcome-patient-footer">
         <img
           className="welcome-patient-virus-logo"
           src={virusLogo}
           alt="Wir VS Virus Logo"/>
-      </div>
+      </footer>
     <div className="welcome-background-overlay"></div>
-  </div>
+  </main>
   );
 }
 
