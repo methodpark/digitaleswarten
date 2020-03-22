7ea7757a87d6263e066b7c3ed87bba82f955f4c3
Gronner <felix.braeunling@t-online.de>
2020-03-22 09:39:53 +0100

    Add file docstring to explain app start up

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,20 @@
 # flask_web/app.py
+"""
+1. Creates the basic flask application
+2. Connects the app to the database
+3. Creates database tables, if they don't exist
+"""
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from pathlib import Path
 import os
 
+app = Flask(__name__)
+
 sqlite_location = '/db/digitalesWartenDB.sqlite'
 if 'SQLITE_LOCATION' in os.environ:
     sqlite_location = os.environ['SQLITE_LOCATION']
-
-app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{sqlite_location}'
 db = SQLAlchemy(app)
