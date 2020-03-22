cbf06bfedbc963b94d733d1e0e9efeccc90c9a4d
Gronner <felix.braeunling@t-online.de>
2020-03-22 12:54:25 +0100

    Remove and add whitespace as necessary

--- backend/utils/handle_get_queries.py
+++ backend/utils/handle_get_queries.py
@@ -3,7 +3,7 @@ def get_entry_detail_level(request):
     """
     Returns wether the detail level of an entry should be short or full.
     """
-    entry_level_detail  = request.args.get('personDetails', None)
+    entry_level_detail = request.args.get('personDetails', None)
     if entry_level_detail  not in ['short', 'full']:
         abort(400)
     return entry_level_detail 
